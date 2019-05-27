b6dea038ff69e7363e809911edbb1e29b4db0b82
michael <ucapmhy@ucl.ac.uk>
2019-05-27 21:36:13 +0100

    Function now iterable over all parameter. Introduces bug (like in hw_sims) where gds files overlap Find how to overwrite them

--- spiral_resonator.py
+++ spiral_resonator.py
@@ -6,19 +6,20 @@ import res_shapes as rs
 import gdspy
 import subprocess
 
-w_cap = 5       #width of capacitor
-w_in = 5        #width of inductor
+w_caps = [5]       #width of capacitor
+w_ins = [5]        #width of inductor
 l_ins = [100]   #length of inductors
 gaps = [5]      #size of gaps
 
-def spiral_resonator_gds(w_cap, w_in, l_ins, gaps):
+def spiral_resonator_gds(w_caps, w_ins, l_ins, gaps):
     poly_cell = gdspy.Cell('POLYGONS')
     folder = os.path.join(ebl,'gavin_spiral/')
-
+    j = 0
     #resonator - 3 element list [coarse res elements, fine res elements, stuff to remove from ground plane]
-    for j in range(len(gaps)*len(l_ins)):
-        l_in = l_ins[j//len(gaps)]
-        gap = gaps[j%len(gaps)]
+    for w_cap,w_in,l_in,gap in [(w_cap,w_in,l_in,gap) for w_cap in w_caps \
+        for w_in in w_ins for l_in in l_ins for gap in gaps]:
+        #l_in = l_ins[j//len(gaps)]
+        #gap = gaps[j%len(gaps)]
         resonator = rs.TRII(w_cap, w_in, l_in, gap)
         
         for i in resonator[0]:
@@ -41,9 +42,10 @@ def spiral_resonator_gds(w_cap, w_in, l_ins, gaps):
         #Write the pattern as a gds file
         os.chdir(folder)
         gdspy.write_gds('spiral_resonator_{}.gds'.format(j), unit=1.0e-6, precision=1.0e-9)
+        j = j + 1
 
 if __name__ == "__main__":
-    spiral_resonator_gds(w_cap,w_in,l_ins,gaps)
+    spiral_resonator_gds(w_caps,w_ins,l_ins,gaps)
     subprocess.run("klayout ~/Documents/group-project--morton/EBL/gavin_spiral/spiral_resonator_0.gds",
         shell=True, check=True,
         executable='/bin/bash')
